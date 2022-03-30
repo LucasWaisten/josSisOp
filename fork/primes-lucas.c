f8fbb193d8961b9bb8f2ec7f57c616e6a7afc733
LucasWaisten <waistenlucas@gmail.com>
2022-03-30 10:14:10 -0300

    #1 primes Lucas terminado, faltan detalles y comentarios

--- fork/primes-lucas.c
+++ fork/primes-lucas.c
@@ -1,58 +1,84 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
+void error(char* message);
 
-int procesoDerecho(int* array,int* fd){
-	/*pensar la condicion de corte*/
+void procesoDerecho(int fd_izquierdo[])
+{
+	int primo = 0;
+	if (read(fd_izquierdo[0], &primo, sizeof(primo)) == 0){
+		close(fd_izquierdo[0]);
+		return;
+	}
+
+	printf("prime %d\n", primo);
+
+	int fds_derecho[2];
+	int r_new = pipe(fds_derecho);
+	if (r_new < 0) error("Error en pipe");
+
+	int i_new = fork();
+	if (i_new < 0) error("Error en fork!");
+
+	else if (i_new == 0){ //nuevo hijo
+		close(fd_izquierdo[0]);
+		close(fds_derecho[1]);
+		procesoDerecho(fds_derecho);
+
+	} else { //nuevo padre
+		close(fds_derecho[0]);
+
+		int n = 0;
+		while (read(fd_izquierdo[0], &n, sizeof(n)) > 0){
+			if (n % primo != 0){
+				if (write(fds_derecho[1], &n, sizeof(n)) < 0) error("Error en el write");
+			}
+		}
+		close(fd_izquierdo[0]);
+		close(fds_derecho[1]);
+		wait(NULL);
+	}
+	return;
 }
 
+int
+main(int argc, char *argv[])
+{
+	int num = atoi(argv[1]); //recibo el número por argumento del main
+	if(num<2) error("Error en numero ingresado");
 
-int main(int argc, char *argv[]){
-	int num = atoi(argv[1]);
-	if(num<2){
-		perror("Error numero ingresado");
-		_exit(0);
-	}
-	int array[num]; 
-	int alectura[num];
+	int fds[2];
+	int r = pipe(fds); //creo un pipe
+	if (r < 0) error("Error en pipe");
 
-	int fd[2];
-	pipe(fd);
-	/* 	fd[0] -> read; 
-		fd[1] -> write
-	*/
-
-	__pid_t pid;
-	pid = fork();
+	int pid = fork(); //hago fork
+	if(pid<0) error("Error en Fork");
 
 	switch (pid)
 	{
 	case 0:
-		/*Primero proceso derecho */
-		close(fd[1]);
-		read(fd[0],alectura,sizeof(alectura));
-		procesoDerecho(alectura,fd);
-		close(fd[0]);
+		close(fds[1]); //hijo no escribe
+		procesoDerecho(fds);
 		break;
-	case -1:
-		/* Error*/
-		break;
-	
 	default:
-		/* Primer Proceso */
-		close(fd[0]);
-		for(int i=0;i<=num;i++){
-			array[i] = i;
-			printf("%d",array[i]);
-		}
+		close(fds[0]); //padre no lee
 
-		write(fd[1],array,sizeof(array));
-
-		close(fd[1]);
+			for (int j = 2; j <= num; j++){ //envío la secuencia de números desde 2 hasta n
+				if (write(fds[1], &j, sizeof(j)) < 0) error("Error en el write");
+			}
+			close(fds[1]);
+			wait(NULL);
 		break;
 	}
-
 	return 0;
+}
 
+void error(char* message){
+	perror(message);
+	_exit(0);	
+	return;
 }
